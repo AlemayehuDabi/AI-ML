a0fe4bd712456d6bbe71ed601ac6623718beb972
Alemayehu <alemayehudabi606@gmail.com>
2026-05-30 18:43:46 +0300

    wip: random num in array generation

---
+++ week2/Day2/random_num_generation.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+rand_arr = np.random.rand(3,4)
+print("Random Array: \n", rand_arr)
+
+rand_arr_int = np.random.randint(0,10, size=(2,3))
+print("Random Array Int: \n", rand_arr_int)
