f0671881c76ce30b2dafe0e87873873a8e2bd1ed
Alemayehu <alemayehudabi606@gmail.com>
2026-06-04 23:35:42 +0300

    wip: concat, merge, join

--- week2/Day4/concat_merge_join.py
+++ week2/Day4/concat_merge_join.py
@@ -1,17 +1,62 @@
 # concat, merge, join
 import pandas as pd
-import numpy as np
+# import numpy as np
 
-df = pd.DataFrame({"Employee": ["Amina", "James", "Priya", "Chen", "Maria", "Omar"],
-    "Age": [29, np.nan, 34, 41, np.nan, 27],
-    "City": ["London", "Birmingham", np.nan, "Manchester", "Leeds", np.nan],
-    "Salary": [52000, 48000, np.nan, 61000, 54000, np.nan],
-    "Join_Date": ["2022-03-14", "2021-11-01", "2023-06-20", np.nan, "2020-09-05", "2024-01-18"]})
+df1 = pd.DataFrame({
+    "id": [1, 2, 3],
+    "name": ["Alice", "Bob", "Cara"],
+    "age": [25, 30, 38]
+})
+
+df2 = pd.DataFrame({
+    "id": [3, 4],
+    "name": ["Cara", "Dan"],
+    "age": [28, 35]
+})
 
 # concat
+# row
+df_row_concat = pd.concat([df1,df2], ignore_index=True)
+# print(df_row_concat)
+
+# col
+df_col_concat = pd.concat([df1, df2], axis=1)
+# print(df_col_concat)
 
 
 # merging
+# inner
+df_merge = df1.merge(df2, how='inner', on=['id', 'name'])
+# print(df_merge)
 
+# left
+df_left_merge = df1.merge(df2, how='left', on=['age'])
+# print(df_left_merge)
 
-# joing
+# right
+df_right_merge = df1.merge(df2, how='right', on=['age'])
+# print(df_right_merge)
+
+# outer
+df_outer_merge = df1.merge(df2, how='outer', on=['age'])
+# print(df_outer_merge)
+
+# joning
+# - this under the hood uses the merge
+
+# new data set
+df_temp = pd.DataFrame({
+    "STATION": ["S1", "S2", "S3"],
+    "TEMP": [15, 20, 22]
+}).set_index("STATION")
+
+df_precip = pd.DataFrame({
+    "STATION": ["S1", "S2", "S3", "S4"],
+    "PRECIP": [10, 12, 8, 15]
+})
+
+# Join on STATION column to df_temp's index
+result = df_precip.join(df_temp, on="STATION", how="left")
+print(result)
+
+# and this is just like the merge same for the how parameter
