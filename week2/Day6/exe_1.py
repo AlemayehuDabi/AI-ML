7aa2b7c60e32969851a10eed4e3699215bbc339b
Alemayehu <alemayehudabi606@gmail.com>
2026-06-07 02:03:53 +0300

    wip: execrise on plots

---
+++ week2/Day6/exe_1.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as ptl
+
+# Create Basic plots w/ Matplotlib
+# plot using line-graph, bar-chart, scatter-plot
+
+# line-graph
+Name = ["Neo", "Jon", "Alicen", "Elijah"]
+Grade = [50, 45, 46, 30]
+
+# ptl.plot(Name, Grade, color='green')
+# ptl.title('Student Grade using line graph')
+# ptl.xlabel('Grade')
+# ptl.ylabel('Name')
+# ptl.show()
+
+# bar-chart
+# ptl.bar(Name, Grade, color='blue')
+# ptl.title('Student Grade using Bar Chart')
+# ptl.xlabel('Grade')
+# ptl.ylabel('Name')
+# ptl.show()
+
+# scatter-plot
+# ptl.scatter(Name, Grade, color='red')
+# ptl.title('Student Grade using Scatter plot')
+# ptl.xlabel('Grade')
+# ptl.ylabel('Name')
+# ptl.show()
