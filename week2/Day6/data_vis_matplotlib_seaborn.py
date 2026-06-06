e673b3eedbdca8898c37d000334c54170be9f88e
Alemayehu <alemayehudabi606@gmail.com>
2026-06-06 23:38:10 +0300

    wip: bar chart

--- week2/Day6/data_vis_matplotlib_seaborn.py
+++ week2/Day6/data_vis_matplotlib_seaborn.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 
 # Basic Matplotlib synthax
-x = [1,2,3,4,5]
-y= [10,20,35,60,75]
+x = ['Ana','Khali','Miko','Josi']
+y= [10,17, 15, 14]
 
-plt.plot(x,y)
-plt.show()
+# plt.plot(x,y)
+# plt.show()
 
 # Line Plot
-plt.plot(x,y,label='Trend')
-plt.title("Line Plot")
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
-plt.legend()
-plt.show()
+# plt.plot(x,y,label='Trend')
+# plt.title("Line Plot")
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+# plt.legend()
+# # plt.show()
+
+# Bar Chart
+# plt.title('Students Age')
+# plt.bar(x,y, color='blue')
+# plt.show()
