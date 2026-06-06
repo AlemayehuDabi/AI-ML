84326ac711e2b34086642285f0a1ac86c605fd6d
Alemayehu <alemayehudabi606@gmail.com>
2026-06-06 23:33:32 +0300

    wip: line plot

--- week2/Day6/data_vis_matplotlib_seaborn.py
+++ week2/Day6/data_vis_matplotlib_seaborn.py
@@ -6,3 +6,11 @@ y= [10,20,35,60,75]
 
 plt.plot(x,y)
 plt.show()
+
+# Line Plot
+plt.plot(x,y,label='Trend')
+plt.title("Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.legend()
+plt.show()
