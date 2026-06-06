a552e4ddd062308a2a10ecf3b21d7a672b58300e
Alemayehu <alemayehudabi606@gmail.com>
2026-06-06 23:53:51 +0300

    wip: scatter plot

--- week2/Day6/data_vis_matplotlib_seaborn.py
+++ week2/Day6/data_vis_matplotlib_seaborn.py
@@ -7,7 +7,7 @@ y= [10,17, 15, 14]
 # plt.plot(x,y)
 # plt.show()
 
-# Line Plot
+# Line Plot - visualize trends overtime
 # plt.plot(x,y,label='Trend')
 # plt.title("Line Plot")
 # plt.xlabel("X-axis")
@@ -15,13 +15,19 @@ y= [10,17, 15, 14]
 # plt.legend()
 # # plt.show()
 
-# Bar Chart
+# Bar Chart - catagories data comparision
 # plt.title('Students Age')
 # plt.bar(x,y, color='blue')
 # plt.show()
 
-# Histogram
-data = [1,2,2,3,3,4,4,4,5,5,5,6,6]
-plt.hist(data, bins=4, color='blue', edgecolor='green')
-plt.title('Histogram')
+# Histogram - shows the distrbution of dataset
+# data = [1,2,2,3,3,4,4,4,5,5,5,6,6]
+# plt.hist(data, bins=4, color='blue', edgecolor='green')
+# plt.title('Histogram')
+# plt.show()
+
+# scatter plot
+plt.scatter(x,y,color='Red') # it is used to visualize the r/ship b/n two cont variables 
+plt.title("Show the potenitial danger area")
+
 plt.show()
