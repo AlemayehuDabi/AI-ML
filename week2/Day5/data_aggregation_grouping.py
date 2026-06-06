3c440f88fcd304fe841d530e7b87c1c041c15afc
Alemayehu <alemayehudabi606@gmail.com>
2026-06-06 21:47:45 +0300

    wip: grouping and aggregation on df

---
+++ week2/Day5/data_aggregation_grouping.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+
+data = {
+    'Region': ['East', 'East', 'West', 'West', 'North', 'North', 'South', 'South'],
+    'Product': ['A', 'B', 'A', 'B', 'A', 'B', 'A', 'B'],
+    'Sales': [150, 200, 300, 250, 180, 220, 270, 310],
+    'Quantity': [10, 15, 20, 25, 12, 18, 22, 28]
+}
+
+df = pd.DataFrame(data)
+
+# group by two cols
+grouped = df.groupby(['Region', 'Product'])
+
+grouped_sum = grouped.sum()
+# print('Grouped Sum: \n', grouped_sum)
+
+grouped_mean = grouped.mean()
+# print('Grouped Mean: \n', grouped_mean)
+
+
+# group by one col - and operate agg on specific col
+group_one_col = df.groupby('Region')
+
+
+do_sum_on_sales = group_one_col['Sales'].sum()
+# print('Do Sum on Sales Col: \n', do_sum_on_sales)
+
+# use agg method
+do_agg_on_group = group_one_col.agg({
+    'Sales': ['mean', 'sum'],
+    'Quantity': ['mean', 'sum']
+})
+
+print('Using Agg funtion: \n', do_agg_on_group)
+
+
+ 
