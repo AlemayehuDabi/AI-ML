77d6f3d4a13759c0bac629f3f3b5535733f7e461
Alemayehu <alemayehudabi606@gmail.com>
2026-06-10 12:12:09 +0300

    wip: scalar, cross, and type of matrix

--- week3/Day1/vector_and_matrix.py
+++ week3/Day1/vector_and_matrix.py
@@ -7,3 +7,25 @@ B = np.array([[5,6], [7,8]])
 # print("Addition: \n", A+B)
 # matrix subtraction
 # print("Subtraction: \n", A-B)
+
+# scalar muliptication
+C = 2 * A
+# print("Mulitplication: \n", C)
+
+# Cross product - there is a method called dot for this.
+cross_product = np.dot(A,B)
+# print(cross_product)
+# A*B is not cross product but instead this is multiplication 
+
+
+# identity matrix
+I = np.eye(3)
+# print("Identity Matrix: \n", I)
+
+# Zero matrix
+Z = np.zeros((4,3))
+# print("Zero Matrix: \n", Z)
+
+# diagonal matrix
+D = np.diag([1,2,4,5])
+# print("Diagnoal Matrix: \n", D)
