f1e9df183f6286ad3a7e9b650cef9ae1bddff6dc
Alemayehu <alemayehudabi606@gmail.com>
2026-06-10 11:38:15 +0300

    wip: matrix addition and subtraction

---
+++ week3/Day1/day1.md
@@ -0,0 +1,2 @@
+### Linear Algebra Fundamental
+**vector and matrix**
---
+++ week3/Day1/vector_and_matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = np.array([[1,2], [3,4]])
+B = np.array([[5,6], [7,8]])
+
+# matrix addition
+# print("Addition: \n", A+B)
+# matrix subtraction
+# print("Subtraction: \n", A-B)
