5dea7f127324207e19b815d23876c961e43f79ee
Alemayehu <alemayehudabi606@gmail.com>
2026-06-10 11:28:12 +0300

    wip: scatter plot

--- week2/Day7/eda.py
+++ week2/Day7/eda.py
@@ -23,13 +23,15 @@ df.drop(columns=["Cabin"], inplace=True)
 # remove duplicate
 df = df.drop_duplicates()
 
+# print(df)
+
 # first class data - 5 row
 first_class = df[df["Pclass"] == 1].head().iloc[:,:3]
-print("First Class: \n", first_class)
+# print("First Class: \n", first_class)
 
 # second class data - 5 row
 second_class = df[df["Pclass"] == 2].head().iloc[:,:3]
-print("Second Class: \n", second_class)
+# print("Second Class: \n", second_class)
 # Generate Visualizations to Illustrate key insights - name, ticket
 # line-plot
 # plt.plot(second_class["PassengerId"], second_class["Survived"], color='blue', label='first and second class trend')
@@ -43,8 +45,15 @@ print("Second Class: \n", second_class)
 # plt.show()
 
 # histo graph
-plt.hist(first_class["Survived"], label="Histograph", bins=10, color='Green')
-plt.title("First Class Titanic Survived Distribution")
-plt.xlabel("Survived Dis")
-plt.ylabel("Frequency Dis")
+# plt.hist(first_class["Survived"], label="Histograph", bins=10, color='Green')
+# plt.title("First Class Titanic Survived Distribution")
+# plt.xlabel("Survived Dis")
+# plt.ylabel("Frequency Dis")
+# plt.show()
+
+# scatter plot
+plt.scatter(df["Age"], df["Fare"], edgecolor="white", color='blue', alpha=0.5)
+plt.title("Age vs Fare")
+plt.xlabel("Age")
+plt.ylabel("Fare")
 plt.show()
