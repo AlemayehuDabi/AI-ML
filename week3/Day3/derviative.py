a0d091dd906b98f38003a857a67fcdec0ae76ada
alemayehudabi <alemayehudabi606@gmail.com>
2026-06-29 15:34:39 +0300

    feat: partial derivative

--- week3/Day3/derviative.py
+++ week3/Day3/derviative.py
@@ -5,3 +5,16 @@ f = x**2
 
 derviative = sp.diff(f, x)
 print("Derviative: \n", derviative)
+
+# partial derivatives
+r = sp.Symbol('r')
+t = sp.Symbol('t')
+
+g = r**3 + t*2
+
+par_deri_r = sp.diff(g,r)
+par_deri_t = sp.diff(g,t)
+
+
+print("Partial Derviative of R: ", par_deri_r)
+print("Partial Derviative of T: ", par_deri_t)
